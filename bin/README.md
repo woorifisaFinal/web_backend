0eed034e943b698468f33b81f4b09497919bf1be
bgmun01 <ansqudrms01@gmail.com>
2023-08-29 18:25:21 +0900

    test

---
+++ bin/README.md
@@ -0,0 +1,7 @@
+# web_backend
+## introduction
+1. Spring Boot 4.0.0  
+2. Java SE 17  
+3. Mysql 8.0
+4. dependency: lombok, Spring DATA JPA
+5. application.properties  - 로컬DB  
--- src/main/java/com/woorifis/demo/controller/BoardController.java
+++ src/main/java/com/woorifis/demo/controller/BoardController.java
@@ -1,5 +1,78 @@
 package com.woorifis.demo.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.dto.BoardDto;
+import com.example.demo.model.entity.Board;
+import com.woorifis.demo.model.service.BoardService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/board")
+@Slf4j
 public class BoardController {
 
+	private BoardService service;
+	
+	public BoardController(BoardService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/regist")
+	public String registForm() {
+		return "board/registboard";
+	}
+		
+	@PostMapping("/regist")
+	public String registBoard(@ModelAttribute BoardDto board) {
+		service.writeBoard(board);
+		return "redirect:/board/list";
+		
+	}
+	
+	@GetMapping("/list")
+	public String list(@RequestParam(required = false, defaultValue =  "1") Integer page, Model model) { //int의 레포타입인 인티저로 해야함 없을수도 있으니까 객체로?
+		// 페이징 처리 - 한페이지에 10 개씩만 보 여주자
+		page--;
+		Page<Board> pageInfo = service.listboard(page);
+		model.addAttribute("pageInfo", pageInfo);
+		
+		//log.debug("page: {}",page);
+		return "board/list";
+
+	}
+	@GetMapping("/detail")
+	public String detail(@RequestParam int no, Model model)	{
+		log.debug("no :{}",no);
+		try {
+			Board board = service.detailBoard(no);
+			model.addAttribute("board", board);
+			return "board/detail";
+		}catch(RuntimeException e) {
+			return "/board/list";
+		}
+	}
+	// a 링크 타고오면 get 매핑
+	@GetMapping("/delete")
+	public String delete(@RequestParam int no) {
+		log.debug("board no:{}", no);
+		service.deleteBoard(no);
+		
+		return"redirect:/board/list";
+	}
+	
+	@PostMapping("/update")
+	public String update(@ModelAttribute BoardDto dto,Model model) {
+		log.debug("board 수정: {}", dto);
+		service.writeBoard(dto);
+		return "redirect:/board/detail?no=" +dto.getNo();
+	}
 }
