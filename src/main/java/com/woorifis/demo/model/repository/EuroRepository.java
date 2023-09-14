3bdcc81dc0a7c31bb297eaa690570ca2b7055c4d
jwjw916 <wjeditor916@gmail.com>
2023-09-14 17:06:49 +0900

    symbol update 0914

---
+++ src/main/java/com/woorifis/demo/model/repository/EuroRepository.java
@@ -0,0 +1,9 @@
+package com.woorifis.demo.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.woorifis.demo.model.entity.Euro;
+
+public interface EuroRepository extends JpaRepository<Euro, String>{
+
+}
---
+++ src/main/java/com/woorifis/demo/model/repository/NikkeiRepository.java
@@ -0,0 +1,11 @@
+package com.woorifis.demo.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.woorifis.demo.model.entity.Nikkei;
+
+
+
+public interface NikkeiRepository extends JpaRepository<Nikkei, String>{
+
+}
